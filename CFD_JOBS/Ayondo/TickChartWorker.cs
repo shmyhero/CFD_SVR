ae74832743c3a5bc30da23f9c5dae4049efa4b49
Ivan Jiang <ivan@tradehero.mobi>
2016-03-17 14:47:49 +0800

    tick chart job

--- CFD_JOBS/Ayondo/TickChartWorker.cs
+++ CFD_JOBS/Ayondo/TickChartWorker.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using CFD_COMMON;
 using CFD_COMMON.Models.Cached;
-using ServiceStack.Redis.Generic;
+using Newtonsoft.Json;
 
 namespace CFD_JOBS.Ayondo
 {
@@ -14,55 +15,106 @@ namespace CFD_JOBS.Ayondo
             var basicRedisClientManager = CFDGlobal.GetBasicRedisClientManager();
             var redisClient = basicRedisClientManager.GetClient();
             var redisQuoteClient = redisClient.As<Quote>();
-            var redisTickClient = redisClient.As<Tick>();
+            //var redisTickClient = redisClient.As<Tick>();
+
+            IList<Quote> lastQuotes = null;
+
+            CFDGlobal.LogLine("Starting...");
 
             while (true)
             {
-                try
+                int overdueCount = 0;
+                int identicalCount = 0;
+                int updateCount = 0;
+                int appendCount = 0;
+                int newCount = 0;
+
+                var quotes = redisQuoteClient.GetAll();
+
+                //skip non-updated quotes to improve speed
+                IList<Quote> updatedQuotes;
+                if (lastQuotes != null)
                 {
-                    var quotes = redisQuoteClient.GetAll();
-
-                    //var tickListNames = redisClient.SearchKeys("tick:*");
-
+                    var idsToRemove = new List<int>();
                     foreach (var quote in quotes)
                     {
-                        //if (DateTime.UtcNow - quote.Time > TimeSpan.FromMinutes(1))
-                        //    continue;
+                        var lastQuote = lastQuotes.FirstOrDefault(o => o.Id == quote.Id);
+                        if (lastQuote != null && lastQuote.Time == quote.Time)
+                            idsToRemove.Add(quote.Id);
+                    }
+                    updatedQuotes = quotes.Where(o => !idsToRemove.Contains(o.Id)).ToList();
+                }
+                else
+                    updatedQuotes = quotes.Select(o => o).ToList();
 
-                        var listName = "tick:" + quote.Id;
-                        IRedisList<Tick> list = redisTickClient.Lists[listName];
+                //remember quotes for next check
+                lastQuotes = quotes.Select(o => o).ToList();
 
-                        if (list.Count == 0)
+                foreach (var quote in updatedQuotes)
+                {
+                    var listName = "tick:" + quote.Id;
+
+                    //IRedisList<Tick> list = redisTickClient.Lists[listName];
+                    //var listCount = list.Count;
+                    var listCount = redisClient.GetListCount(listName);
+
+                    var newTick = new Tick()
+                    {
+                        P = quote.Offer,
+                        Time = quote.Time
+                    };
+
+                    if (listCount == 0) //new quote?
+                    {
+                        //CFDGlobal.LogLine(quote.Id+" new");
+                        newCount++;
+                        //list.Add(newTick);
+                        redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick));
+                        continue;
+                    }
+
+                    //var lastTick = list[(int) listCount - 1]; //last tick in cache
+                    var lastTick = JsonConvert.DeserializeObject<Tick>(redisClient.GetItemFromList(listName, (int) listCount - 1)); //last tick in cache
+                    if (newTick.Time > lastTick.Time)
+                    {
+                        if (IsEqualDownToMinute(newTick.Time, lastTick.Time))
                         {
-                            list.Add(new Tick() {P = quote.Offer, Time = quote.Time});
-                            continue;
+                            //CFDGlobal.LogLine(quote.Id + " update");
+                            updateCount++;
+                            //list[(int) listCount - 1] = newTick; //update last tick to new tick
+                            redisClient.SetItemInList(listName, (int) listCount - 1, JsonConvert.SerializeObject(newTick)); //update last tick to new tick
                         }
-
-                        //var list = redisTickClient.Lists["tick:" + quote.Id];
-
-                        //if(list.)
-
-                        var last = list.Last();
-
-                        var o = quote.Time - last.Time;
-
-                        //if(last.Time)
-
-                        //var tick = new Tick() { P = quote.Offer, Time = quote.Time };
-                        //list.Add(tick);
-
-                        //var all = list.GetAll();
-
-                        //l.Add(tick);
+                        else
+                        {
+                            //CFDGlobal.LogLine(quote.Id + " append");
+                            appendCount++;
+                            //list.Add(newTick); //append new tick
+                            redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick)); //append new tick
+                        }
+                    }
+                    else
+                    {
+                        if (newTick.Time == lastTick.Time)
+                            identicalCount++;
+                        else
+                        //CFDGlobal.LogLine(quote.Id + " skip");
+                            overdueCount++;
                     }
                 }
-                catch (Exception e)
-                {
-                    CFDGlobal.LogException(e);
-                }
 
+                CFDGlobal.LogLine("updatedQuotes: " + updatedQuotes.Count +
+                                  " update: " + updateCount + " append: " + appendCount + " identical: " + identicalCount + " new: " + newCount + " overdue: " + overdueCount);
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
+        }
+
+        private static bool IsEqualDownToMinute(DateTime t1, DateTime t2)
+        {
+            return t1.Minute == t2.Minute
+                   && t1.Hour == t2.Hour
+                   && t1.Day == t2.Day
+                   && t1.Month == t2.Month
+                   && t1.Year == t2.Year;
         }
     }
 }
