11a322894e386dacbc144fb93e28c6b35dbfacab
Ivan Jiang <ivan@tradehero.mobi>
2016-04-08 16:08:36 +0800

    add try catch

--- CFD_JOBS/Ayondo/AyondoFixFeedWorker.cs
+++ CFD_JOBS/Ayondo/AyondoFixFeedWorker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CFD_COMMON;
 using CFD_COMMON.Models.Cached;
@@ -28,83 +29,90 @@ namespace CFD_JOBS.Ayondo
                 //System.Console.WriteLine("o hai");
                 System.Threading.Thread.Sleep(1000);
 
-                //save ProdDefs
-                if (!myApp.ProdDefs.IsEmpty)
+                try
                 {
-                    //CFDGlobal.LogLine("Pending ProdDefs detected. Loading from queue...");
-
-                    //new prod list from Ayondo MDS2
-                    IList<ProdDef> listNew = new List<ProdDef>();
-
-                    while (!myApp.ProdDefs.IsEmpty)
+                    //save ProdDefs
+                    if (!myApp.ProdDefs.IsEmpty)
                     {
-                        ProdDef obj;
-                        var tryDequeue = myApp.ProdDefs.TryDequeue(out obj);
-                        listNew.Add(obj);
-                    }
+                        //CFDGlobal.LogLine("Pending ProdDefs detected. Loading from queue...");
 
-                    CFDGlobal.LogLine("Saving " + listNew.Count + " ProdDefs to Redis...");
+                        //new prod list from Ayondo MDS2
+                        IList<ProdDef> listNew = new List<ProdDef>();
 
-                    //current redis list
-                    var listOld = redisProdDefClient.GetAll();
-
-                    IList<ProdDef> listToSave = new List<ProdDef>();
-
-                    foreach (var newProdDef in listNew)
-                    {
-                        var old = listOld.FirstOrDefault(o => o.Id == newProdDef.Id);
-
-                        if (old != null) //updating prod def in redis
+                        while (!myApp.ProdDefs.IsEmpty)
                         {
-                            //update open/close time/price depending on state change
-                            if (old.QuoteType != enmQuoteType.Closed && newProdDef.QuoteType == enmQuoteType.Closed) //xxx -> close
-                            {
-                                CFDGlobal.LogLine("PROD CLOSED " + newProdDef.Id + " time: " + newProdDef.Time);
-
-                                //close time
-                                old.LastClose = newProdDef.Time;
-                            }
-                            else if (old.QuoteType != enmQuoteType.Open && newProdDef.QuoteType == enmQuoteType.Open) //xxx -> open
-                            {
-                                CFDGlobal.LogLine("PROD OPENED " + newProdDef.Id + " time: " + newProdDef.Time + " offer: " + newProdDef.Offer + " bid: " + newProdDef.Bid);
-
-                                //open time
-                                old.LastOpen = newProdDef.Time;
-
-                                //open prices
-                                old.OpenAsk = newProdDef.Offer;
-                                old.OpenBid = newProdDef.Bid;
-
-                                //preclose
-                                old.PreClose = newProdDef.CloseAsk;
-                            }
-
-                            //update fields
-                            old.Time = newProdDef.Time;
-                            old.QuoteType = newProdDef.QuoteType;
-                            old.Name = newProdDef.Name;
-                            old.Symbol = newProdDef.Symbol;
-                            old.AssetClass = newProdDef.AssetClass;
-                            old.Bid = newProdDef.Bid;
-                            old.Offer = newProdDef.Offer;
-                            old.CloseBid = newProdDef.CloseBid;
-                            old.CloseAsk = newProdDef.CloseAsk;
-                            old.Shortable = newProdDef.Shortable;
-                            old.MinSizeShort = newProdDef.MinSizeShort;
-                            old.MaxSizeShort = newProdDef.MaxSizeShort;
-                            old.MinSizeLong = newProdDef.MinSizeLong;
-                            old.MaxSizeLong = newProdDef.MaxSizeLong;
-                            old.MaxLeverage = newProdDef.MaxLeverage;
-
-                            listToSave.Add(old);
+                            ProdDef obj;
+                            var tryDequeue = myApp.ProdDefs.TryDequeue(out obj);
+                            listNew.Add(obj);
                         }
-                        else //appending new prod def into redis
+
+                        CFDGlobal.LogLine("Saving " + listNew.Count + " ProdDefs to Redis...");
+
+                        //current redis list
+                        var listOld = redisProdDefClient.GetAll();
+
+                        IList<ProdDef> listToSave = new List<ProdDef>();
+
+                        foreach (var newProdDef in listNew)
                         {
-                            listToSave.Add(newProdDef);
-                        }
-                    }
+                            var old = listOld.FirstOrDefault(o => o.Id == newProdDef.Id);
 
-                    redisProdDefClient.StoreAll(listToSave);
+                            if (old != null) //updating prod def in redis
+                            {
+                                //update open/close time/price depending on state change
+                                if (old.QuoteType != enmQuoteType.Closed && newProdDef.QuoteType == enmQuoteType.Closed) //xxx -> close
+                                {
+                                    CFDGlobal.LogLine("PROD CLOSED " + newProdDef.Id + " time: " + newProdDef.Time);
+
+                                    //close time
+                                    old.LastClose = newProdDef.Time;
+                                }
+                                else if (old.QuoteType != enmQuoteType.Open && newProdDef.QuoteType == enmQuoteType.Open) //xxx -> open
+                                {
+                                    CFDGlobal.LogLine("PROD OPENED " + newProdDef.Id + " time: " + newProdDef.Time + " offer: " + newProdDef.Offer + " bid: " + newProdDef.Bid);
+
+                                    //open time
+                                    old.LastOpen = newProdDef.Time;
+
+                                    //open prices
+                                    old.OpenAsk = newProdDef.Offer;
+                                    old.OpenBid = newProdDef.Bid;
+
+                                    //preclose
+                                    old.PreClose = newProdDef.CloseAsk;
+                                }
+
+                                //update fields
+                                old.Time = newProdDef.Time;
+                                old.QuoteType = newProdDef.QuoteType;
+                                old.Name = newProdDef.Name;
+                                old.Symbol = newProdDef.Symbol;
+                                old.AssetClass = newProdDef.AssetClass;
+                                old.Bid = newProdDef.Bid;
+                                old.Offer = newProdDef.Offer;
+                                old.CloseBid = newProdDef.CloseBid;
+                                old.CloseAsk = newProdDef.CloseAsk;
+                                old.Shortable = newProdDef.Shortable;
+                                old.MinSizeShort = newProdDef.MinSizeShort;
+                                old.MaxSizeShort = newProdDef.MaxSizeShort;
+                                old.MinSizeLong = newProdDef.MinSizeLong;
+                                old.MaxSizeLong = newProdDef.MaxSizeLong;
+                                old.MaxLeverage = newProdDef.MaxLeverage;
+
+                                listToSave.Add(old);
+                            }
+                            else //appending new prod def into redis
+                            {
+                                listToSave.Add(newProdDef);
+                            }
+                        }
+
+                        redisProdDefClient.StoreAll(listToSave);
+                    }
+                }
+                catch (Exception e)
+                {
+                    CFDGlobal.LogException(e);
                 }
             }
             //initiator.Stop();
--- CFD_JOBS/Ayondo/TickChartWorker.cs
+++ CFD_JOBS/Ayondo/TickChartWorker.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Threading;
 using CFD_COMMON;
 using CFD_COMMON.Models.Cached;
-using Newtonsoft.Json;
 using ServiceStack.Redis.Generic;
 
 namespace CFD_JOBS.Ayondo
 {
     public class TickChartWorker
     {
-        private static int CLEAR_HISTORY_WHEN_SIZE = 60 * 24 * 10;//xx days' most possible count
-        private static int CLEAR_HISTORY_TO_SIZE = 60 * 24 * 7;//xx days' most possible count
+        private static int CLEAR_HISTORY_WHEN_SIZE = 60*24*10; //xx days' most possible count
+        private static int CLEAR_HISTORY_TO_SIZE = 60*24*7; //xx days' most possible count
 
         public static void Run()
         {
@@ -27,97 +26,105 @@ namespace CFD_JOBS.Ayondo
 
             while (true)
             {
-                int overdueCount = 0;
-                int identicalCount = 0;
-                int updateCount = 0;
-                int appendCount = 0;
-                int newCount = 0;
-
-                var quotes = redisQuoteClient.GetAll();
-
-                //skip non-updated quotes to improve speed
-                IList<Quote> updatedQuotes;
-                if (lastQuotes != null)
+                try
                 {
-                    var idsToRemove = new List<int>();
-                    foreach (var quote in quotes)
+                    int overdueCount = 0;
+                    int identicalCount = 0;
+                    int updateCount = 0;
+                    int appendCount = 0;
+                    int newCount = 0;
+
+                    var quotes = redisQuoteClient.GetAll();
+
+                    //skip non-updated quotes to improve speed
+                    IList<Quote> updatedQuotes;
+                    if (lastQuotes != null)
                     {
-                        var lastQuote = lastQuotes.FirstOrDefault(o => o.Id == quote.Id);
-                        if (lastQuote != null && lastQuote.Time == quote.Time)
-                            idsToRemove.Add(quote.Id);
-                    }
-                    updatedQuotes = quotes.Where(o => !idsToRemove.Contains(o.Id)).ToList();
-                }
-                else
-                    updatedQuotes = quotes.Select(o => o).ToList();
-
-                //remember quotes for next check
-                lastQuotes = quotes.Select(o => o).ToList();
-
-                foreach (var quote in updatedQuotes)
-                {
-                    var listName = "tick:" + quote.Id;
-
-                    IRedisList<Tick> list = redisTickClient.Lists[listName];
-                    //var listCount = list.Count;
-                    //var listCount = redisClient.GetListCount(listName);
-
-                    var newTick = new Tick()
-                    {
-                        P = quote.Offer,
-                        Time = quote.Time
-                    };
-
-                    if (list.Count == 0) //new quote?
-                    {
-                        //CFDGlobal.LogLine(quote.Id+" new");
-                        newCount++;
-                        list.Add(newTick);
-                        //redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick));
-                        continue;
-                    }
-
-                    //clear history/prevent data increasing for good
-                    if (list.Count > CLEAR_HISTORY_WHEN_SIZE) //data count at most possible size (in x days )
-                    {
-                        CFDGlobal.LogLine(quote.Id + " Clearing data from " + list.Count + " to " + CLEAR_HISTORY_TO_SIZE);
-                        var ticks = list.GetAll();
-                        var newTicks = ticks.Skip(ticks.Count - CLEAR_HISTORY_TO_SIZE);
-                        list.RemoveAll();
-                        list.AddRange(newTicks);
-                    }
-
-                    var lastTick = list[list.Count - 1]; //last tick in cache
-                    //var lastTick = JsonConvert.DeserializeObject<Tick>(redisClient.GetItemFromList(listName, (int) listCount - 1)); //last tick in cache
-                    if (newTick.Time > lastTick.Time)
-                    {
-                        if (IsEqualDownToMinute(newTick.Time, lastTick.Time))
+                        var idsToRemove = new List<int>();
+                        foreach (var quote in quotes)
                         {
-                            //CFDGlobal.LogLine(quote.Id + " update");
-                            updateCount++;
-                            list[list.Count - 1] = newTick; //update last tick to new tick
-                            //redisClient.SetItemInList(listName, (int) listCount - 1, JsonConvert.SerializeObject(newTick)); //update last tick to new tick
+                            var lastQuote = lastQuotes.FirstOrDefault(o => o.Id == quote.Id);
+                            if (lastQuote != null && lastQuote.Time == quote.Time)
+                                idsToRemove.Add(quote.Id);
                         }
-                        else
-                        {
-                            //CFDGlobal.LogLine(quote.Id + " append");
-                            appendCount++;
-                            list.Add(newTick); //append new tick
-//                            redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick)); //append new tick
-                        }
+                        updatedQuotes = quotes.Where(o => !idsToRemove.Contains(o.Id)).ToList();
                     }
                     else
+                        updatedQuotes = quotes.Select(o => o).ToList();
+
+                    //remember quotes for next check
+                    lastQuotes = quotes.Select(o => o).ToList();
+
+                    foreach (var quote in updatedQuotes)
                     {
-                        if (newTick.Time == lastTick.Time)
-                            identicalCount++;
+                        var listName = "tick:" + quote.Id;
+
+                        IRedisList<Tick> list = redisTickClient.Lists[listName];
+                        //var listCount = list.Count;
+                        //var listCount = redisClient.GetListCount(listName);
+
+                        var newTick = new Tick()
+                        {
+                            P = quote.Offer,
+                            Time = quote.Time
+                        };
+
+                        if (list.Count == 0) //new quote?
+                        {
+                            //CFDGlobal.LogLine(quote.Id+" new");
+                            newCount++;
+                            list.Add(newTick);
+                            //redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick));
+                            continue;
+                        }
+
+                        //clear history/prevent data increasing for good
+                        if (list.Count > CLEAR_HISTORY_WHEN_SIZE) //data count at most possible size (in x days )
+                        {
+                            CFDGlobal.LogLine(quote.Id + " Clearing data from " + list.Count + " to " + CLEAR_HISTORY_TO_SIZE);
+                            var ticks = list.GetAll();
+                            var newTicks = ticks.Skip(ticks.Count - CLEAR_HISTORY_TO_SIZE);
+                            list.RemoveAll();
+                            list.AddRange(newTicks);
+                        }
+
+                        var lastTick = list[list.Count - 1]; //last tick in cache
+                        //var lastTick = JsonConvert.DeserializeObject<Tick>(redisClient.GetItemFromList(listName, (int) listCount - 1)); //last tick in cache
+                        if (newTick.Time > lastTick.Time)
+                        {
+                            if (IsEqualDownToMinute(newTick.Time, lastTick.Time))
+                            {
+                                //CFDGlobal.LogLine(quote.Id + " update");
+                                updateCount++;
+                                list[list.Count - 1] = newTick; //update last tick to new tick
+                                //redisClient.SetItemInList(listName, (int) listCount - 1, JsonConvert.SerializeObject(newTick)); //update last tick to new tick
+                            }
+                            else
+                            {
+                                //CFDGlobal.LogLine(quote.Id + " append");
+                                appendCount++;
+                                list.Add(newTick); //append new tick
+//                            redisClient.AddItemToList(listName, JsonConvert.SerializeObject(newTick)); //append new tick
+                            }
+                        }
                         else
-                        //CFDGlobal.LogLine(quote.Id + " skip");
-                            overdueCount++;
+                        {
+                            if (newTick.Time == lastTick.Time)
+                                identicalCount++;
+                            else
+                            //CFDGlobal.LogLine(quote.Id + " skip");
+                                overdueCount++;
+                        }
                     }
+
+                    CFDGlobal.LogLine("updatedQuotes: " + updatedQuotes.Count +
+                                      " update: " + updateCount + " append: " + appendCount + " identical: " + identicalCount + " new: " + newCount + " overdue: " + overdueCount);
+                }
+                catch (Exception e)
+                {
+                    CFDGlobal.LogException(e);
                 }
 
-                CFDGlobal.LogLine("updatedQuotes: " + updatedQuotes.Count +
-                                  " update: " + updateCount + " append: " + appendCount + " identical: " + identicalCount + " new: " + newCount + " overdue: " + overdueCount);
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
         }
