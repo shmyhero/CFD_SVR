81eaf09a7f76d09fd0fea777315008e7e1b8aeea
Ivan Jiang <ivan@tradehero.mobi>
2016-04-12 12:28:14 +0800

    add AyondoTrade worker

---
+++ AyondoTrade/WebRole.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace AyondoTrade
+{
+    public class WebRole : RoleEntryPoint
+    {
+        public override bool OnStart()
+        {
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            return base.OnStart();
+        }
+
+        public override void Run()
+        {
+            base.Run();
+        }
+
+        public override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
