ea569956e98f48e9875913a81788954bf25b138b
Ivan Jiang <ivan@tradehero.mobi>
2016-05-26 14:32:32 +0800

    alert message via websocket

--- CFD_API/SignalR/PositionReportTicker.cs
+++ CFD_API/SignalR/PositionReportTicker.cs
@@ -70,24 +70,30 @@ namespace CFD_API.SignalR
                             var secIds = dicUserPositionReports.SelectMany(o => o.Value.Select(p => Convert.ToInt32(p.SecurityID))).Distinct().ToList();
                             var prodDefs = _redisClient.GetByIds(secIds);
 
-                            foreach (var pair in dicUserPositionReports)
+                            foreach (var pair in dicUserPositionReports) //for every ayondo username
                             {
-                                Clients.Group(pair.Key).p(pair.Value.Select(report =>
+                                string connectionId = null;
+                                var tryGetValue = _subscription.TryGetValue(pair.Key, out connectionId);
+
+                                if (tryGetValue)
                                 {
-                                    var secId = Convert.ToInt32(report.SecurityID);
-                                    var prodDef = prodDefs.FirstOrDefault(o => o.Id == secId);
-                                    var name = Translator.GetCName(prodDef.Name);
-                                    var stopTake = report.Text == "Position DELETE by StopLossOrder" ? "止损" : "止盈";
-                                    var price = Math.Round(report.SettlPrice, prodDef.Prec);
-                                    var pl = Math.Round(report.PL.Value, 2);
-                                    return "您够买的" + name + "已被" + stopTake + "在" + price + "，收益为" + pl + "美元";
-                                }));
+                                    Clients.Group(connectionId).p(pair.Value.Select(report =>
+                                    {
+                                        var secId = Convert.ToInt32(report.SecurityID);
+                                        var prodDef = prodDefs.FirstOrDefault(o => o.Id == secId);
+                                        var name = Translator.GetCName(prodDef.Name);
+                                        var stopTake = report.Text == "Position DELETE by StopLossOrder" ? "止损" : "止盈";
+                                        var price = Math.Round(report.SettlPrice, prodDef.Prec);
+                                        var pl = Math.Round(report.PL.Value, 2);
+                                        return "您够买的" + name + "已被" + stopTake + "在" + price + "，收益为" + pl + "美元";
+                                    }));
+                                }
                             }
                         }
                     }
                     catch (Exception e)
                     {
-                       CFDGlobal.LogException(e);
+                        CFDGlobal.LogException(e);
                     }
                 }
 
