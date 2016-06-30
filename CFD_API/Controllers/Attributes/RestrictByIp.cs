1b05093904065e635206c44e6634a014f0f52ed7
Ivan Jiang <ivan@tradehero.mobi>
2016-06-30 10:44:53 +0800

    ip restriction filter

---
+++ CFD_API/Controllers/Attributes/RestrictByIp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CFD_API.Controllers.Attributes
+{
+    public class RestrictByIp:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+
+            string ip = null;
+            if (actionContext.Request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                var requestBase = ((HttpContextWrapper)actionContext.Request.Properties["MS_HttpContext"]).Request;
+                ip = requestBase.UserHostAddress;
+            }
+        }
+    }
+}
