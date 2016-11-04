2055b2ea36818c2df38b20080f89bc0687f41a77
Andy <andy@tradehero.mobi>
2016-11-04 16:46:59 +0800

    Win App for Jobs

---
+++ CFD_JOBS_WinApp/Converter/StatusConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CFD_JOBS_WinApp.Converter
+{
+    public class StatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRunning = (bool)value;
+
+            if (isRunning)
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ CFD_JOBS_WinApp/Interface/BaseCFDJob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFD_JOBS_WinApp.Interface
+{
+    public abstract class BaseCFDJob : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string JobName { get; set; }
+
+        private DateTime createdAt;
+        public DateTime CreatedAt {
+            get {
+                return createdAt;
+            }
+            set {
+                createdAt = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CreatedAt"));
+            }
+        }
+
+        private string exceptionMessage;
+        public string ExceptionMessage { get {
+                return exceptionMessage;
+            }
+            set {
+                exceptionMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ExceptionMessage"));
+            }
+        }
+
+        protected List<string> logInfoItems = new List<string>();
+
+        private string logInfo;
+        public string LogInfo {
+            get { return logInfo; }
+            set { logInfo = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LogInfo")); }
+        }
+
+        private bool isRunning = false;
+        public bool IsRunning {
+            get { return isRunning; }
+            set { isRunning = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning")); }
+        }
+    }
+}
