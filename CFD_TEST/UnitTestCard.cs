f6bdc679c9f6fbb29f951e150ba5a65216d94240
Andy <andy@tradehero.mobi>
2016-12-07 15:16:11 +0800

    获取卡片时增加是否有效的判断

---
+++ CFD_TEST/UnitTestCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CFD_COMMON.Service;
+using CFD_COMMON.Models.Context;
+
+namespace CFD_TEST
+{
+    [TestClass]
+    public class UnitTestCard
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var db = CFDEntities.Create())
+            {
+                CardService svr = new CardService(db);
+                var cardList = svr.GetCard(10M, 20M, 100);
+            }
+            
+        }
+    }
+}
