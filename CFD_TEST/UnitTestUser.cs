ed61978b7cd1f0b120e2bb331dff2b573a8e63c2
Andy <andy@tradehero.mobi>
2016-12-12 16:47:20 +0800

    出金的修改，增加测试代码

--- CFD_TEST/UnitTestUser.cs
+++ CFD_TEST/UnitTestUser.cs
@@ -44,5 +44,46 @@ namespace CFD_TEST
                 request.Abort();
             }
         }
+
+        [TestMethod]
+        public void PushToken()
+        {
+            string requestStr = "{\"deviceToken\":\"f60c5d5a898b1c19cb6e5d58520c8906\",\"deviceType\":1}";
+            byte[] binaryData = Encoding.UTF8.GetBytes(requestStr);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.typhoontechnology.hk/api/user/pushtokenauth");
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11033/api/user/pushtokenauth");
+
+            try
+            {
+                request.ContentType = "application/json";
+                request.Method = "POST";
+                request.Headers.Add("Authorization", "Basic 2031_b58b17d610c747d5a03356b03565719d");
+                request.ContentLength = binaryData.Length;
+                request.Timeout = int.MaxValue;
+                Stream reqstream = request.GetRequestStream();
+                reqstream.Write(binaryData, 0, binaryData.Length);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream streamReceive = response.GetResponseStream();
+                Encoding encoding = Encoding.UTF8;
+
+                StreamReader streamReader = new StreamReader(streamReceive, encoding);
+                string strResult = streamReader.ReadToEnd();
+                Console.WriteLine(strResult);
+                return;
+            }
+            catch (WebException webEx)
+            {
+                throw new WebException("操作超时");
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                request.Abort();
+            }
+        }
     }
 }
