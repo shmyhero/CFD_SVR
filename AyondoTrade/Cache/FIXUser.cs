aef5d2c46505e04efca1b69e6f539ee97eb27cae
Ivan Jiang <jiangyi_1985@hotmail.com>
2017-01-25 10:21:29 +0800

    fix caching model

---
+++ AyondoTrade/Cache/FIXUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickFix.FIX44;
+
+namespace AyondoTrade.Cache
+{
+    public class FIXUser
+    {
+        public string Account { get; set; }
+
+        public DateTime? LastLoginAt { get; set; }
+
+        public decimal? Balance { get; set; }
+        public IList<PositionReport> OpenPositions { get; set; }
+        public IList<PositionReport> ClosedPositions { get; set; }
+    }
+}
