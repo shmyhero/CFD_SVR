a7fc3a0eadbc02dea91e1ffd27607c3b2ae88d21
Ivan Jiang <jiangyi_1985@hotmail.com>
2017-03-16 18:22:34 +0800

    return all user for QA test

--- CFD_API/Controllers/RankController.cs
+++ CFD_API/Controllers/RankController.cs
@@ -64,11 +64,11 @@ namespace CFD_API.Controllers
                 });
             }
 
-            ////100 at max
-            //userDTOs = userDTOs.Take(100).ToList();
+            ////only return users with positive ROIs
+            //var result = userDTOs.Take(1).Concat(userDTOs.Skip(1).Where(o => o.roi > 0).Take(99)).ToList();
 
-            //only return users with positive ROIs
-            var result = userDTOs.Take(1).Concat(userDTOs.Skip(1).Where(o => o.roi > 0).Take(99)).ToList();
+            //for test only
+            var result = userDTOs;
 
             //populate nickname/picUrl
             var userIds = result.Select(o => o.id).ToList();
