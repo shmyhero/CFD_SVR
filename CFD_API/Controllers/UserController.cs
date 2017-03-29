d954a66bc378baa59fcd0b08f9bbdea683bab50e
Andy <andy@tradehero.mobi>
2017-03-29 14:28:59 +0800

    颜色字段前面加#

--- CFD_API/Controllers/UserController.cs
+++ CFD_API/Controllers/UserController.cs
@@ -1910,13 +1910,13 @@ namespace CFD_API.Controllers
             Tuple<string, string> result = new Tuple<string, string>(string.Empty,string.Empty);
             switch(transType.ToLower().Trim())
             {
-                case "eft": result = new Tuple<string, string>("出金", "000000"); break;
-                case "wecollect - cup": result = new Tuple<string, string>("入金", "1c8d13"); break;
-                case "bank wire": result = new Tuple<string, string>("交易金入金", "1c8d13"); break;
-                case "transaction fee": result = new Tuple<string, string>("手续费", "000000"); break;
-                case "trade result": result = new Tuple<string, string>("交易", string.Empty); break;
-                case "financing": result = new Tuple<string, string>("隔夜费", string.Empty); break;
-                case "dividend": result = new Tuple<string, string>("分红", string.Empty); break;
+                case "eft": result = new Tuple<string, string>("出金", "#000000"); break;
+                case "wecollect - cup": result = new Tuple<string, string>("入金", "#1c8d13"); break;
+                case "bank wire": result = new Tuple<string, string>("交易金入金", "#1c8d13"); break;
+                case "transaction fee": result = new Tuple<string, string>("手续费", "#000000"); break;
+                case "trade result": result = new Tuple<string, string>("交易", "#000000"); break;
+                case "financing": result = new Tuple<string, string>("隔夜费", "#000000"); break;
+                case "dividend": result = new Tuple<string, string>("分红", "#000000"); break;
             }
 
             return result;
