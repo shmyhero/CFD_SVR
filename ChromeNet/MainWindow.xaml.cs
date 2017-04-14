755cff55a7ddcc803a56e2446147430edab277e4
Andy <andy@tradehero.mobi>
2017-04-14 16:51:27 +0800

    Chrome .Net实现访问trade hub接口。

---
+++ ChromeNet/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChromeNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChromiumWebBrowser webView = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var setting = new CefSharp.CefSettings();
+            CefSharp.Cef.Initialize(setting);
+
+            webView = new CefSharp.Wpf.ChromiumWebBrowser();
+            borderChrome.Child = webView;
+            webView.FrameLoadEnd += WebView_FrameLoadEnd;
+
+            txtUrl.Text = "https://www.baidu.com";
+        }
+
+        private void WebView_FrameLoadEnd(object sender, CefSharp.FrameLoadEndEventArgs e)
+        {
+            if (e.Frame.IsMain)
+            {
+                //e.Frame.ViewSource();
+                e.Frame.GetTextAsync().ContinueWith(task => {
+                    var html = task.Result;
+                    MessageBox.Show("返回字符串总长度:" + html.Length.ToString());
+                });
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webView.Address = txtUrl.Text;
+        }
+    }
+}
