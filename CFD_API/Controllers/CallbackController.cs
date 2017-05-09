a56084071a47ed29a310384f73b1968a1e1abd2f
Andy <andy@tradehero.mobi>
2017-05-09 17:29:55 +0800

    的 -> 地

--- CFD_API/Controllers/CallbackController.cs
+++ CFD_API/Controllers/CallbackController.cs
@@ -390,9 +390,9 @@ namespace CFD_API.Controllers
                     msg.Title = "绑卡消息";
                     if (form.status == BankCardUpdateStatus.Approved)
                     {
-                        YunPianMessenger.SendSms("【盈交易】恭喜您！您的银行卡绑定成功了，可以尽情的出金了哟！", user.Phone);
+                        YunPianMessenger.SendSms("【盈交易】恭喜您！您的银行卡绑定成功了，可以尽情地出金了哟！", user.Phone);
 
-                        msg.Body = "恭喜您！您的银行卡绑定成功了，可以尽情的出⾦了哟！";
+                        msg.Body = "恭喜您！您的银行卡绑定成功了，可以尽情地出⾦了哟！";
                         msg.CreatedAt = DateTime.UtcNow;
                         msg.IsReaded = false;
                     }
