3a73f912335d04eda2aa184a5b9a3b012f98edbd
Andy <andy@tradehero.mobi>
2017-07-31 17:23:07 +0800

    实盘开通判断条件改为ApproveAt不为空

--- CFD_API/Controllers/CommonController.cs
+++ CFD_API/Controllers/CommonController.cs
@@ -209,7 +209,7 @@ namespace CFD_API.Controllers
             ActivityDTO dto = new ActivityDTO();
 
             var user = GetUser();
-            if(string.IsNullOrEmpty(user.AyLiveUsername))
+            if(user.AyLiveApproveAt != null)
             {
                 var demoAct = db.Miscs.FirstOrDefault(m=>m.Key=="DemoActivity");
                 if(demoAct!=null)
