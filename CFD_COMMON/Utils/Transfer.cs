943cfabe5efcc7938f0955b377958e25765f04d3
Andy <andy@tradehero.mobi>
2017-09-11 15:02:27 +0800

    修改出入金列表颜色

--- CFD_COMMON/Utils/Transfer.cs
+++ CFD_COMMON/Utils/Transfer.cs
@@ -28,10 +28,10 @@ namespace CFD_COMMON.Utils
                     break;
                 case "wecollect - cup":
                 case "adyen - skrill":
-                    result = new Tuple<string, string>("入金", "#1c8d13");
+                    result = new Tuple<string, string>("入金", "#000000");
                     break;
                 case "bank wire":
-                    result = new Tuple<string, string>("交易金入金", "#1c8d13");
+                    result = new Tuple<string, string>("交易金入金", "#000000");
                     break;
                 case "transaction fee":
                     result = new Tuple<string, string>("手续费", "#000000");
